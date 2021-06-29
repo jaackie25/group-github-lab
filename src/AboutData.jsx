8acec2f04e4c92246372459c87512b9b9cd4012f
die-perez <pzandie@gmail.com>
2021-06-29 13:56:14 -0700

    andie stuff added

--- src/AboutData.jsx
+++ src/AboutData.jsx
@@ -1,11 +1,18 @@
-import ryan from './image/ryan.jpeg'
+import andie from './image/andie.jpeg'
 
 const aboutData = [  { 
 
   name: 'ryan kim',
   blurb: 'i am ryan',
   img: 
-  <img className="portImage" src={ryan} alt='ryan'
+  // <img className="portImage" src={golfgame} alt='golf'
+  />
+}, 
+{ 
+  name: 'andie perez',
+  blurb: 'lover of all things dark & moody',
+  img: 
+  <img className="portImage" src={andie} alt='andrea perez'
   />
 },
 
