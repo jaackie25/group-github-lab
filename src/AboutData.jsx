ba776dcfb460ad0a35f73ea6ca2b30d3c69309a6
SpiralV <MVelasco90@hotmail.com>
2021-06-29 13:50:43 -0700

    add picture and array

--- src/AboutData.jsx
+++ src/AboutData.jsx
@@ -1,12 +1,15 @@
 const AboutData = [  { 
 
   name: 'ryan kim',
-  blurb: 'i am ryan',
-  img: 
-  // <img className="portImage" src={golfgame} alt='golf'
-  />
-},
+  blurb: 'i am ryan'
+}
 
+][
+{
+  name: "Matthew Velasco",
+  blurb: "Matthew Velasco is a rookie coder looking to break it in big to the hotshot coding industry. Matthew has 9 weeks of professional training and experience in the field of computer coding. Matthew is a proud owner of one Roborovski Hamster. Matthew doesn't know what else to add to this but I'm paddin it out.",
+  img: <img href="https://ca.slack-edge.com/T0351JZQ0-U01UY7R9S3D-a75ff571ee69-512"></img>
+}
 ]
 
 export default AboutData
