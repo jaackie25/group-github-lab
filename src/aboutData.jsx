0c40086b9ef7bd68a4d65bb378e36a9342360c20
jaackie25 <jackiedinh91@gmail.com>
2021-06-29 14:40:16 -0700

    idk what im doing

---
+++ src/aboutData.jsx
@@ -0,0 +1,22 @@
+import ryan from './image/ryan.jpeg'
+import jackie from './image/jackie.jpg'
+
+const aboutData = [  { 
+
+  name: 'ryan kim',
+  blurb: 'i am ryan',
+  img: 
+  <img className="portImage" src={ryan} alt='ryan'
+  />
+},
+{
+  name: 'Jackie Dinh',
+  blurb: 'My name is Jackie Dinh. I enjoy snowboarding and paddle boarding',
+  img: 
+  <img className="portImage" src={jackie} alt='jackie'
+   />
+},
+
+]
+
+export default aboutData
